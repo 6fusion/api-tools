9ef9c9be1aa4c9e988ff70465227c22212aafb24
Fernanda Coto <fermarioro@gmail.com>
2016-03-17 09:46:01 -0600

    Fix indentation

--- ruby/meter-health/csv_formatter.rb
+++ ruby/meter-health/csv_formatter.rb
@@ -1,26 +1,27 @@
 require 'csv'
 require_relative 'helper_methods.rb'
+
 class CsvFormatter
 
-	include HelperMethods
+  include HelperMethods
 
-	def initialize(headers)
-		@headers = headers
-		@credentials = retrieve_credentials
-		create_file_with_headers
-	end
+  def initialize(headers)
+    @headers = headers
+    @credentials = retrieve_credentials
+    create_file_with_headers
+  end
 
-	def write(info)
-		CSV.open(@credentials["file_name"], "a+") do |csv|
+  def write(info)
+    CSV.open(@credentials["file_name"], "a+") do |csv|
       csv << info
     end
   end
 
-	private
+  private
 
-	def create_file_with_headers
-		CSV.open(@credentials["file_name"], "w+") do |csv|
+  def create_file_with_headers
+    CSV.open(@credentials["file_name"], "w+") do |csv|
       csv << @headers
     end
-	end
+  end
 end
